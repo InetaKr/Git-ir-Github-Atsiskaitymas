a3ff2774f434d5c4c903ebb33fe5481e1740e5c2
InetaKr <ineta.kriunaite@outlook.com>
2023-01-09 16:57:39 +0200

    modified js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
-
+// Get a reference to the buttons and the tab content elements
 const buttons = document.querySelectorAll('.formButtons button');
 const tabContent = document.querySelectorAll('.tab-panel');
 
+// Add click event listeners to all of the buttons
 buttons.forEach(button => {
   button.addEventListener('click', () => {
+    // Get the ID of the button that was clicked
     const buttonId = button.getAttribute('id');
 
+    // Hide all of the tab content elements
     tabContent.forEach(content => {
       content.style.display = 'none';
     });
 
-    document.querySelector(`#${buttonId}Tab`).style.display = 'flex';
+    // Show the desired tab content element
+    const selectedTab = document.querySelector(`#${buttonId}Tab`);
+    selectedTab.style.display = 'block';
+
+    // Add a click event listener to the close icon in the selected tab
+    const closeIcon = selectedTab.querySelector('.fa-close');
+    closeIcon.addEventListener('click', () => {
+      // Hide the selected tab
+      selectedTab.style.display = 'none';
+    });
   });
 });
